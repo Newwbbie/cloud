a4a9f4ff7786d14ba2277aa1b22910862c30e40a
1098160709 <1098160709@qq.com>
2021-01-29 17:19:52 +0800

    到P53

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/newwbbie/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,21 @@
+package com.newwbbie.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @auther wb
+ * @create 2021/1/29
+ **/
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "----PaymentFallbackService fall back paymentInfo_OK,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_Timeout(Integer id) {
+        return "----PaymentFallbackService fall back paymentInfo_Timeout,o(╥﹏╥)o";
+    }
+}
